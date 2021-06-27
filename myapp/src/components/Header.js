3ca444d019fb9de69d11b22f3fc867c3c57055af
Roberta Meireles <robertameireles.r@gmail.com>
2021-06-27 17:21:27 +0100

    created logOut

--- myapp/src/components/Header.js
+++ myapp/src/components/Header.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react"
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import AuthService from "../services/auth.service"
 
 
 function Header () {
+      const logOut = () => {
+        AuthService.logout()
+        window.location.href = "/"
+      };
+
     return (
         <Navbar fixed="top" bg="light" expand="lg">
             <Navbar.Brand href="/">Project Products</Navbar.Brand>
@@ -13,7 +19,7 @@ function Header () {
                 <Nav.Link className="nav-bar" href="/products">Products</Nav.Link>
                 <Nav.Link className="nav-bar" href="/customers">Customers</Nav.Link>
                 <Nav.Link className="nav-bar" href="/invoices">Invoice</Nav.Link>
-                <Nav.Link className="nav-bar" href="/">Logout</Nav.Link>
+                <Nav.Link className="nav-bar" href="/" onClick={logOut} >Logout</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
--- myapp/src/components/SideBar.js
+++ myapp/src/components/SideBar.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import Nav from 'react-bootstrap/Nav'
+import AuthService from "../services/auth.service"
 
 const SideBar = () => {
+    const logOut = () => {
+        AuthService.logout()
+        window.location.href = "/"
+      };
     return (
         <div className="side" >
             <Nav defaultActiveKey="/home" className="flex-column">
                 <Nav.Link className="side-bar" href="/products">Products <i className="fas fa-shopping-cart"></i></Nav.Link>
                 <Nav.Link className="side-bar" href="/customers">Customers <i className="fas fa-user"></i></Nav.Link>
                 <Nav.Link className="side-bar" href="/invoices">Invoices <i className="fas fa-file-invoice-dollar"></i></Nav.Link>
-                <Nav.Link className="side-bar" href="/">Logout <i className="fas fa-sign-out-alt"></i></Nav.Link>
+                <Nav.Link className="side-bar" href="/" onClick={logOut} >Logout<i className="fas fa-sign-out-alt"></i></Nav.Link>
             </Nav>
         </div>
     )
